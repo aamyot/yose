db8623603ff423ccae2e84d39938616e4994d839
Alexandre Amyot <alexandre.amyot@gmail.com>
2015-02-16 16:46:49 -0500

    refactor test

--- src/test/java/com/alexandreamyot/yose/primes/PrimesTest.java
+++ src/test/java/com/alexandreamyot/yose/primes/PrimesTest.java
@@ -9,11 +9,9 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.nio.file.Paths;
 
-import static java.nio.file.Files.readAllBytes;
 import static java.nio.file.Paths.get;
 import static org.junit.Assert.assertThat;
 import static org.testinfected.hamcrest.dom.DomMatchers.hasUniqueSelector;
@@ -22,28 +20,19 @@ public class PrimesTest {
 
     @Test
     public void uiShouldIncludeAHtmlForm() {
-        String html = view("primes");
-        assertThat(loadDocumentElement(html), hasUniqueSelector("form", hasUniqueSelector("#title"),
-                                                                        hasUniqueSelector("#invitation"),
-                                                                        hasUniqueSelector("input#number[type='text']"),
-                                                                        hasUniqueSelector("button#go")));
+        assertThat(view("primes"), hasUniqueSelector("form", hasUniqueSelector("#title"),
+                                                             hasUniqueSelector("#invitation"),
+                                                             hasUniqueSelector("input#number[type='text']"),
+                                                             hasUniqueSelector("button#go")));
     }
 
-    private Element loadDocumentElement(String html) {
+    private Element view(String viewname) {
         try {
+            FileReader reader = new FileReader(get("src/main/webapp/views/" + viewname + ".html").toFile());
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            return documentBuilder.parse(new InputSource(new StringReader(html))).getDocumentElement();
+            return documentBuilder.parse(new InputSource(reader)).getDocumentElement();
         } catch (ParserConfigurationException | SAXException | IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private String view(String name) {
-        try {
-            ;
-            return new String(readAllBytes(get(Paths.get("src/main/webapp/views/" + name + ".html").toUri())));
-        } catch (Exception e) {
-            throw new InternalError(e);
         }
     }
 }
