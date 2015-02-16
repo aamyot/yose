aacb5fc9368dd3d08615d84c5cd28c26894d8943
Alexandre Amyot <alexandre.amyot@gmail.com>
2015-02-15 22:21:22 -0500

    refactor handle method

--- src/main/java/com/alexandreamyot/yose/web/Primes.java
+++ src/main/java/com/alexandreamyot/yose/web/Primes.java
@@ -10,44 +10,38 @@ import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
 import com.vtence.molecule.http.MimeTypes;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.alexandreamyot.yose.primes.Pythagoras.primesOf;
 import static com.vtence.molecule.http.HttpStatus.OK;
 import static java.lang.Integer.parseInt;
+import static java.util.stream.Collectors.toList;
 
 public class Primes implements Application {
 
     @Override
     public void handle(Request request, Response response) throws Exception {
-        response.body(toJson(decompose(request)));
+        List<PrimesResult> results = request.parameters("number").stream().map(this::decompose).collect(toList());
+        response.body(toJson(results));
         response.contentType(MimeTypes.JSON);
         response.status(OK);
     }
 
-    private List<PrimesResult> decompose(Request request) {
-        List<PrimesResult> results = new ArrayList<>();
-
-        List<String> inputs = request.parameters("number");
-        for (String input : inputs) {
-            if (NotANumber.check(input)) {
-                results.add(new NotANumber(input));
-            } else if (NumberIsTooBig.check(input)) {
-                results.add(new NumberIsTooBig(input));
-            } else {
-                results.add(new ValidResult(input, primesOf(parseInt(input))));
-            }
+    private PrimesResult decompose(String input) {
+        if (NotANumber.check(input)) {
+            return new NotANumber(input);
+        } else if (NumberIsTooBig.check(input)) {
+            return new NumberIsTooBig(input);
         }
 
-        return results;
+        return new ValidResult(input, primesOf(parseInt(input)));
     }
 
     private String toJson(List<PrimesResult> results) {
-        return new Gson().toJson(resultOf(results));
+        return new Gson().toJson(firstOrList(results));
     }
 
-    private Object resultOf(List<PrimesResult> results) {
+    private Object firstOrList(List<PrimesResult> results) {
         return results.size() > 1 ? results : results.get(0);
     }
 
