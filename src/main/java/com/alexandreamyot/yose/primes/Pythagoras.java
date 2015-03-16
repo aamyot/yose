29bbe471dbaa3edde5af16c967ba056bf01cfcc0
Alexandre Amyot <alexandre.amyot@gmail.com>
2015-03-16 18:04:21 -0400

    refactor to use functional programing

--- src/main/java/com/alexandreamyot/yose/primes/Pythagoras.java
+++ src/main/java/com/alexandreamyot/yose/primes/Pythagoras.java
@@ -2,17 +2,26 @@ package com.alexandreamyot.yose.primes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.emptyList;
 
 public class Pythagoras {
 
     public static List<Integer> primesOf(int number) {
-        List<Integer> primes = new ArrayList<>();
+        return IntStream.rangeClosed(2, number)
+                        .filter(candidate -> number % candidate == 0)
+                        .mapToObj(subPrimesOf(number))
+                        .findFirst().orElse(emptyList());
+    }
 
-        for (int prime = 2; number > 1; prime++) {
-            for (;number % prime == 0; number /= prime) {
-                primes.add(prime);
-            }
-        }
-        return primes;
+    private static IntFunction<List<Integer>> subPrimesOf(int number) {
+        return candidate -> {
+            List<Integer> primes = new ArrayList<>();
+            primes.add(candidate);
+            primes.addAll(primesOf(number / candidate));
+            return primes;
+        };
     }
 }
