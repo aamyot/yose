5ae0a3be3e9ab072604576fc9150333842fef084
Alexandre Amyot <alexandre.amyot@gmail.com>
2015-03-16 16:31:30 -0400

    refactor to use recursivity

--- src/main/java/com/alexandreamyot/yose/primes/Scribe.java
+++ src/main/java/com/alexandreamyot/yose/primes/Scribe.java
@@ -4,66 +4,57 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import static java.lang.Integer.valueOf;
-
 public class Scribe {
 
-    public static Integer romanToArabic(String input) {
-        Integer acc = valueOf(0);
+    static Map<String, Integer> ROMAN_DICTIONARY = new LinkedHashMap<String, Integer>() {{
+        put("M", 1000);
+        put("CM", 900);
+        put("D", 500);
+        put("CD", 400);
+        put("C", 100);
+        put("XC", 90);
+        put("L", 50);
+        put("XL", 40);
+        put("X", 10);
+        put("IX", 9);
+        put("V", 5);
+        put("IV", 4);
+        put("I", 1);
+    }};
 
-        for (Entry<String, Integer> entry : RomanDictionnary.toArabic().entrySet()) {
-            String roman = entry.getKey();
-            while (input.startsWith(roman)) {
-                acc += entry.getValue();
-                input = input.substring(roman.length());
+    public static int romanToArabic(String input) {
+        int acc = 0;
+
+        if (input.isEmpty()) {
+            return acc;
+        }
+
+        for (Entry<String, Integer> entry : ROMAN_DICTIONARY.entrySet()) {
+            if (input.startsWith(entry.getKey())) {
+                acc += entry.getValue() + romanToArabic(input.substring(entry.getKey().length()));
+                break;
             }
         }
 
         return acc;
+
     }
 
     public static String arabicToRoman(int number) {
         String acc = "";
 
-        for (Entry<Integer, String> entry : RomanDictionnary.fromArabic().entrySet()) {
-            int arabic = entry.getKey();
-            while (number >= arabic) {
-                acc += entry.getValue();
-                number -= arabic;
+        if (number < 1) {
+            return acc;
+        }
+
+        for (Entry<String, Integer> entry : ROMAN_DICTIONARY.entrySet()) {
+            if (number >= entry.getValue()) {
+                acc += entry.getKey() + arabicToRoman(number - entry.getValue());
+                break;
             }
         }
 
         return acc;
-    }
 
-    private static class RomanDictionnary {
-        static Map<String, Integer> DICTIONARY = new LinkedHashMap<String, Integer>() {{
-            put("M", 1000);
-            put("CM", 900);
-            put("D", 500);
-            put("CD", 400);
-            put("C", 100);
-            put("XC", 90);
-            put("L", 50);
-            put("XL", 40);
-            put("X", 10);
-            put("IX", 9);
-            put("V", 5);
-            put("IV", 4);
-            put("I", 1);
-        }};
-
-        static Map<String, Integer> toArabic() {
-            return DICTIONARY;
-        }
-
-        static Map<Integer, String> fromArabic() {
-            Map<Integer, String> arabicToRoman = new LinkedHashMap<>();
-            for (Entry<String, Integer> entry : DICTIONARY.entrySet()) {
-                arabicToRoman.put(entry.getValue(), entry.getKey());
-            }
-            return arabicToRoman;
-
-        }
     }
 }
