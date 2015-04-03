e034a152699d175ce0bc6462b47a959c36762275
Alexandre Amyot <alexandre.amyot@gmail.com>
2015-04-03 16:19:43 -0400

    restarting Minesweeper world... not completed yet

--- src/test/js/minesweeper.render.spec.js
+++ src/test/js/minesweeper.render.spec.js
@@ -1,55 +1,126 @@
 require('../../../webapp/js/minesweeper');
-require('./minesweeper.utils');
 
-describe("Minesweeper", function () {
+describe('Minesweeper', function () {
 
     beforeEach(function() {
         var html = require('fs').readFileSync('./webapp/views/minesweeper.html').toString();
         document = require('domino').createWindow(html).document;
-
-        var grid =  [
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'bomb' , 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'bomb',  'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty']
-        ];
-        board = new minesweeper.Board(grid);
-        board.render();
     });
 
-    describe("Rendering the grid", function() {
+    describe('Rendering', function() {
 
-        it('displays a 8x8 grid', function() {
+        var board;
+
+        beforeEach(function() {
+            board = new minesweeper.Board();
+        });
+
+        it('creates an 8x8 grid if no grid is provided', function () {
+            board.render(document.querySelector('#minesweeper-grid'));
+
             for (var row = 1; row <= 8; row++) {
                 for (var col = 1; col <= 8; col++) {
-                    expect(utils.cell(row, col)).toBeDefined();
+                    expect(cellAt(row, col)).toBeDefined();
                 }
             }
         });
 
-        it('calls the reset function', function() {
+        it('resets the container', function () {
             spyOn(board, 'reset');
 
-            board.render();
+            board.render(document.querySelector('#minesweeper-grid'));
 
-            expect(board.reset).toHaveBeenCalled();
+            expect(board.reset).toHaveBeenCalledWith(document.querySelector('#minesweeper-grid'))
+        });
+    });
+
+    describe('Rendering an injected grid', function() {
+
+        beforeEach(function() {
+            grid = [
+                ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty']
+            ];
+
+            board = new minesweeper.Board(grid);
+            board.render(document.querySelector('#minesweeper-grid'));
         });
 
-        describe("Resetting the board", function() {
+        it('loads a injected grid', function() {
 
-            it(' clears the HTML', function() {
-                board.reset();
-
-                expect(document.querySelector("#minesweeper-grid").innerHTML).toEqual("");
+            grid.forEach(function(line, row) {
+                line.forEach(function(cell, col) {
+                    expect(board.cellAt(row, col)).toEqual(grid[row][col]);
+                })
             });
         });
 
+        describe('Actions', function() {
+
+            it('fails when a cell is trapped', function() {
+
+                clickOnCell(2, 2);
+
+                expect(document.querySelector('#cell-2x2').className).toEqual('lost');
+            });
+
+            it('shows bombs around when a cell is safe', function() {
+
+                clickOnCell(1, 1);
+
+                expect(document.querySelector('#cell-1x1').className).toEqual('safe');
+                expect(document.querySelector('#cell-1x1').innerHTML).toEqual('1');
+            });
+
+        });
+
+        describe('Open fields', function() {
+
+            it('does not display 0 when there is no bomb around', function() {
+                clickOnCell(4, 4);
+
+                expect(document.querySelector('#cell-4x4').innerHTML).toEqual('');
+            });
+
+            it('reveals open field when a cell has no bomb', function() {
+                var grid =  [
+                    ['bomb' , 'empty', 'empty'],
+                    ['empty', 'empty', 'empty'],
+                    ['empty', 'empty', 'bomb' ]
+                ];
+
+                var board = new minesweeper.Board(grid);
+                board.render(document.querySelector('#minesweeper-grid'));
+
+                clickOnCell(3, 1);
+
+                expect(document.querySelector('#cell-2x1').className).toEqual('safe');
+                expect(document.querySelector('#cell-2x1').innerHTML).toEqual('1');
+
+                expect(document.querySelector('#cell-2x2').className).toEqual('safe');
+                expect(document.querySelector('#cell-2x2').innerHTML).toEqual('2');
+
+                expect(document.querySelector('#cell-3x2').className).toEqual('safe');
+                expect(document.querySelector('#cell-3x2').innerHTML).toEqual('1');
+            });
+        });
     });
 
+    var cellAt = function(row, col) {
+        return document.querySelector('#cell-' + row + 'x' + col);
+    };
+
+    var clickOnCell = function(row, col) {
+        var click = document.createEvent('Event');
+        click.initEvent('click', true, true);
+        cellAt(row, col).dispatchEvent(click);
+    };
 
 });
 
--- webapp/js/minesweeper.js
+++ webapp/js/minesweeper.js
@@ -1,147 +1,109 @@
 minesweeper = {};
 
 minesweeper.Board = function (grid) {
-    this.matrix = this.load(grid || this.generateGrid());
+    this.grid = grid || this.generateGrid();
 };
 
 minesweeper.Board.prototype = {
+    render: function (container) {
+        this.reset(container);
 
-    load: function (grid) {
-        console.log(grid);
-        var matrix = [[]];
-        for (var row = 0; row < grid.length; row++) {
-            matrix.push([]);
-            for (var col = 0; col < grid[row].length; col++) {
-                matrix[row].push(this.createCell(row, col , grid[row][col]));
-            }
-        }
-        console.log(matrix);
-        return matrix;
+        this.grid.forEach(function (line, row) {
+            var tr = document.createElement('tr');
+            container.appendChild(tr);
+
+            line.forEach(function (cell, col) {
+                tr.appendChild(this.createCell(row, col));
+            }.bind(this));
+        }.bind(this));
+
     },
 
-    createCell: function(row, col, state) {
-        var cell = new minesweeper.Cell(row, col, state);
-        cell.addOnClick(this);
-        return cell;
-    },
-
-    render: function () {
-        this.reset();
-
-        var container = document.querySelector("#minesweeper-grid");
-        for (var row = 0; row < this.matrix.length; row++) {
-            var line = container.appendChild(document.createElement("tr"));
-            for (var col = 0; col < this.matrix[row].length; col++) {
-                line.appendChild(this.matrix[row][col].elem);
-            }
-        }
+    cellAt: function (row, col) {
+        return this.grid[row][col];
     },
 
     generateGrid: function () {
-        var grid = [[]];
+        var grid = [];
+
         for (var row = 0; row < 8; row++) {
             grid.push([]);
             for (var col = 0; col < 8; col++) {
-                grid[row].push("empty");
+                grid[row].push('empty');
             }
         }
 
         return grid;
     },
 
-    reset: function () {
-        document.querySelector("#minesweeper-grid").innerHTML = "";
-    },
-
-    cellAt: function (row, col) {
-        return this.matrix[row][col];
-    },
-
-    cellExists: function(row, col) {
-        return row >= 0 && row < this.matrix.length &&
-            col >= 0 && col < this.matrix[row].length;
-    }
-
-
-};
-
-minesweeper.Cell = function (row, col, state) {
-    this.row = row;
-    this.col = col;
-    this.elem = this.createElement();
-    this.state = state;
-};
-
-minesweeper.Cell.prototype = {
-
-    createElement: function () {
-        var td = document.createElement("td");
-        td.setAttribute("id", this.id());
-        return td;
-    },
-
-    addOnClick: function(board) {
-        this.elem.addEventListener('click', function () {
-            this.show(board);
+    createCell: function (row, col) {
+        var cell = document.createElement('td');
+        cell.setAttribute('id', this.cellId(row, col));
+        cell.addEventListener('click', function () {
+            this.reveal(row, col);
         }.bind(this));
+        return cell;
     },
 
-    show: function(board) {
-        if (this.state == "bomb") {
-            this.markAsFail();
+    cellId: function(row, col) {
+        return 'cell-' + (row + 1) + 'x' + (col + 1);
+    },
+
+    cellIsValid: function(row, col) {
+        return (row >=0 && row < this.grid.length) &&
+            (col >= 0 && col < this.grid[row].length);
+    },
+
+    cellElementAt: function(row, col) {
+        return document.querySelector('#cell-' + (row + 1) + 'x' + (col + 1));
+    },
+
+    reset: function (container) {
+        container.innerHTML = '';
+    },
+
+    reveal: function (row, col) {
+        var cell = this.cellElementAt(row, col);
+
+        if (this.isTrapped(row, col)) {
+            cell.className = 'lost';
             return;
         }
 
-        this.markAsSafe(board);
+        cell.className = 'safe';
 
-        if (this.bombsAround(board) == 0) {
-            this.neighbor(board)
-                .filter(function(cell) { return cell.elem.className == "" })
-                .forEach(function(cell) { cell.show(board) });
-        }
-
-    },
-
-    markAsFail: function () {
-        this.elem.setAttribute("class", "lost")
-    },
-
-    markAsSafe: function (board) {
-        this.elem.setAttribute("class", "safe");
-
-        var bombs =  this.bombsAround(board);
+        var bombs = this.bombsAround(row, col);
         if (bombs > 0) {
-            this.elem.innerHTML = bombs;
+            cell.innerHTML = bombs;
+        } else {
+            this.neighbours(row, col)
+                .filter(function(point) { return !this.isRevealed(point[0], point[1]); }.bind(this))
+                .forEach(function(point) { this.reveal(point[0], point[1]); }.bind(this));
         }
     },
 
-    id: function () {
-        return "cell-" + (this.row + 1) + "x" + (this.col + 1);
+    bombsAround: function (row, col) {
+        return this.neighbours(row, col)
+            .filter(function(point) { return this.isTrapped(point[0], point[1]); }.bind(this)).length;
     },
 
-    bombsAround: function (board) {
-        var bombsCount = 0;
-        this.neighbor(board)
-            .filter(function(cell) { return cell.isTrapped() })
-            .forEach(function() { bombsCount++; });
-
-        return bombsCount;
-    },
-
-    neighbor: function (board) {
+    neighbours: function (row, col) {
         var around = [
             [-1, -1], [-1, 0], [-1, +1],
             [ 0, -1],          [ 0, +1],
             [+1, -1], [+1, 0], [+1, +1]
         ];
 
-        return around.map(function (offset) { return {x: this.row + offset[0], y: this.col + offset[1]} }.bind(this))
-                     .filter(function (point) { return board.cellExists(point.x, point.y); })
-                     .map(function (point) { return board.cellAt(point.x, point.y); });
+        return around.map(function(offset) { return [row+offset[0], col+offset[1]]; }.bind(this))
+            .filter(function(point) { return this.cellIsValid(point[0], point[1]); }.bind(this));
     },
 
-    isTrapped: function () {
-        return this.state == "bomb";
+    isTrapped: function (row, col) {
+        return this.cellAt(row, col) == 'bomb';
+    },
+
+    isRevealed: function (row, col) {
+        return this.cellElementAt(row, col).className != '';
     }
 };
 
