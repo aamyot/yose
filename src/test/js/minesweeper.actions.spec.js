2320b4ea66d747d5c694c9f4b4838075b5a6140b
Alexandre Amyot <alexandre.amyot@gmail.com>
2015-04-07 15:03:56 -0400

    Introduce concept of Cell to wrap related concerns

---
+++ src/test/js/minesweeper.actions.spec.js
@@ -0,0 +1,78 @@
+require('../../../webapp/js/minesweeper');
+
+describe('Actions:', function() {
+
+    beforeEach(function() {
+        var html = require('fs').readFileSync('./webapp/views/minesweeper.html').toString();
+        document = require('domino').createWindow(html).document;
+
+        var grid = [
+            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty']
+        ];
+
+        var board = new minesweeper.Board(grid);
+        board.render(document.querySelector('#minesweeper-grid'));
+
+    });
+
+    describe('Actions', function() {
+
+        it('fails when a cell is trapped', function() {
+
+            clickOnCell(2, 2);
+
+            expect(document.querySelector('#cell-2x2').className).toEqual('lost');
+        });
+
+        it('shows bombs around when a cell is safe', function() {
+
+            clickOnCell(1, 1);
+
+            expect(document.querySelector('#cell-1x1').className).toEqual('safe');
+            expect(document.querySelector('#cell-1x1').innerHTML).toEqual('1');
+        });
+
+        it('does not display 0 when there is no bomb around', function() {
+            clickOnCell(4, 4);
+
+            expect(document.querySelector('#cell-4x4').innerHTML).toEqual('');
+        });
+
+        it('reveals open field when a cell has no bomb', function() {
+            var grid =  [
+                ['bomb' , 'empty', 'empty'],
+                ['empty', 'empty', 'empty'],
+                ['empty', 'empty', 'bomb' ]
+            ];
+
+            var board = new minesweeper.Board(grid);
+            board.render(document.querySelector('#minesweeper-grid'));
+
+            clickOnCell(3, 1);
+
+            expect(document.querySelector('#cell-2x1').className).toEqual('safe');
+            expect(document.querySelector('#cell-2x1').innerHTML).toEqual('1');
+
+            expect(document.querySelector('#cell-2x2').className).toEqual('safe');
+            expect(document.querySelector('#cell-2x2').innerHTML).toEqual('2');
+
+            expect(document.querySelector('#cell-3x2').className).toEqual('safe');
+            expect(document.querySelector('#cell-3x2').innerHTML).toEqual('1');
+        });
+
+    });
+
+    var clickOnCell = function(row, col) {
+        var click = document.createEvent('Event');
+        click.initEvent('click', true, true);
+        document.querySelector('#cell-' + row + 'x' + col).dispatchEvent(click);
+    };
+
+});
--- src/test/js/minesweeper.render.spec.js
+++ src/test/js/minesweeper.render.spec.js
@@ -7,7 +7,7 @@ describe('Minesweeper', function () {
         document = require('domino').createWindow(html).document;
     });
 
-    describe('Rendering', function() {
+    describe('empty grid', function() {
 
         var board;
 
@@ -34,92 +34,25 @@ describe('Minesweeper', function () {
         });
     });
 
-    describe('Rendering an injected grid', function() {
+    it('loads a injected grid', function() {
+        var grid = [
+            ['empty', 'empty', 'empty'],
+            ['empty', 'bomb' , 'empty'],
+            ['empty', 'bomb' , 'empty']
+        ];
 
-        beforeEach(function() {
-            grid = [
-                ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-                ['empty', 'bomb' , 'empty', 'empty', 'empty', 'empty', 'empty', 'empty']
-            ];
+        var board = new minesweeper.Board(grid);
+        board.render(document.querySelector('#minesweeper-grid'));
 
-            board = new minesweeper.Board(grid);
-            board.render(document.querySelector('#minesweeper-grid'));
-        });
-
-        it('loads a injected grid', function() {
-
-            grid.forEach(function(line, row) {
-                line.forEach(function(cell, col) {
-                    expect(board.cellAt(row, col)).toEqual(grid[row][col]);
-                })
-            });
-        });
-
-        describe('Actions', function() {
-
-            it('fails when a cell is trapped', function() {
-
-                clickOnCell(2, 2);
-
-                expect(document.querySelector('#cell-2x2').className).toEqual('lost');
-            });
-
-            it('shows bombs around when a cell is safe', function() {
-
-                clickOnCell(1, 1);
-
-                expect(document.querySelector('#cell-1x1').className).toEqual('safe');
-                expect(document.querySelector('#cell-1x1').innerHTML).toEqual('1');
-            });
-
-        });
-
-        describe('Open fields', function() {
-
-            it('does not display 0 when there is no bomb around', function() {
-                clickOnCell(4, 4);
-
-                expect(document.querySelector('#cell-4x4').innerHTML).toEqual('');
-            });
-
-            it('reveals open field when a cell has no bomb', function() {
-                var grid =  [
-                    ['bomb' , 'empty', 'empty'],
-                    ['empty', 'empty', 'empty'],
-                    ['empty', 'empty', 'bomb' ]
-                ];
-
-                var board = new minesweeper.Board(grid);
-                board.render(document.querySelector('#minesweeper-grid'));
-
-                clickOnCell(3, 1);
-
-                expect(document.querySelector('#cell-2x1').className).toEqual('safe');
-                expect(document.querySelector('#cell-2x1').innerHTML).toEqual('1');
-
-                expect(document.querySelector('#cell-2x2').className).toEqual('safe');
-                expect(document.querySelector('#cell-2x2').innerHTML).toEqual('2');
-
-                expect(document.querySelector('#cell-3x2').className).toEqual('safe');
-                expect(document.querySelector('#cell-3x2').innerHTML).toEqual('1');
-            });
+        grid.forEach(function(line, row) {
+            line.forEach(function(cell, col) {
+                expect(board.cellAt(row, col).state).toEqual(grid[row][col]);
+            })
         });
     });
 
     var cellAt = function(row, col) {
         return document.querySelector('#cell-' + row + 'x' + col);
-    };
-
-    var clickOnCell = function(row, col) {
-        var click = document.createEvent('Event');
-        click.initEvent('click', true, true);
-        cellAt(row, col).dispatchEvent(click);
     };
 
 });
